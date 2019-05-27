182a519656fa138678bcdeb918b12263072662b8
이재화 <cenacolo34@gmail.com>
2019-05-27 17:10:55 +0900

    스탑워치 상태 처리 로직 개선

--- app/src/main/java/omarbradley/com/stopwatchapp/viewmodel/MainViewModel.kt
+++ app/src/main/java/omarbradley/com/stopwatchapp/viewmodel/MainViewModel.kt
@@ -3,10 +3,10 @@ package omarbradley.com.stopwatchapp.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import omarbradley.com.domain.usecase.stopwatch.StopwatchLogicUsecase
-import omarbradley.com.domain.usecase.stopwatch.StopwatchState
 import omarbradley.com.stopwatchapp.R
 import omarbradley.com.util.base.BaseViewModel
 import omarbradley.com.util.date.HHmmssFormatString
+import kotlin.properties.Delegates
 
 const val STOPWATCH_DELAY_TIME = 10L
 
@@ -20,46 +20,67 @@ class MainViewModel(
     private val _rightButtonTextRes = MutableLiveData<Int>()
     val rightButtonTextRes: LiveData<Int> = _rightButtonTextRes
 
-    private val _isEnableRightButton = MutableLiveData<Boolean>()
-    val isEnableRightButton: LiveData<Boolean> = _isEnableRightButton
+    private val _isEnableLeftButton = MutableLiveData<Boolean>()
+    val isEnableLeftButton: LiveData<Boolean> = _isEnableLeftButton
 
     private val _leftButtonTextRes = MutableLiveData<Int>()
     val leftButtonTextRes: LiveData<Int> = _leftButtonTextRes
 
+    private var stopwatchButtonState: StopwatchButtonState
+            by Delegates.observable(StopwatchButtonState.INIT) { _, _, newValue ->
+                when (newValue) {
+                    StopwatchButtonState.INIT -> {
+                        _isEnableLeftButton.value = false
+                        _timeText.value = "00:00:00.0"
+                        _rightButtonTextRes.value = R.string.label_start
+                        _leftButtonTextRes.value = R.string.label_init
+                    }
+                    StopwatchButtonState.START, StopwatchButtonState.CONTINUE -> {
+                        _isEnableLeftButton.value = true
+                        _rightButtonTextRes.value = R.string.label_stop
+                        _leftButtonTextRes.value = R.string.label_init
+                        stopwatchLogicUseCase.runStopwatch(coroutineScope, STOPWATCH_DELAY_TIME) { milliseconds ->
+                            _timeText.value = milliseconds.HHmmssFormatString
+                        }
+                    }
+                    StopwatchButtonState.RESET -> {
+                        _rightButtonTextRes.value = R.string.label_start
+                        _leftButtonTextRes.value = R.string.label_init
+                        _isEnableLeftButton.value = false
+                        stopwatchLogicUseCase.resetStopwatch(coroutineScope) { milliseconds ->
+                            _timeText.value = milliseconds.HHmmssFormatString
+                        }
+                    }
+                    StopwatchButtonState.STOP -> {
+                        _rightButtonTextRes.value = R.string.label_continue
+                        _leftButtonTextRes.value = R.string.label_init
+                        _isEnableLeftButton.value = true
+                        stopwatchLogicUseCase.stopStopwatch(coroutineScope)
+                    }
+                }
+            }
+
     init {
-        _timeText.value = "00:00:00"
-        _isEnableRightButton.value = true
-        _rightButtonTextRes.value = R.string.label_start
-        _leftButtonTextRes.value = R.string.label_init
+        stopwatchButtonState = StopwatchButtonState.INIT
     }
 
-    fun onClickRightButton() = with(stopwatchLogicUseCase) {
-        when (stopwatchState) {
-            StopwatchState.START -> {
-                _rightButtonTextRes.value = R.string.label_continue
-                stopStopwatch(coroutineScope)
-            }
-            StopwatchState.STOP -> {
-                _rightButtonTextRes.value = R.string.label_stop
-                runStopwatch(coroutineScope, STOPWATCH_DELAY_TIME) { milliseconds ->
-                    _timeText.value = milliseconds.HHmmssFormatString
-                }
-            }
-            StopwatchState.RESET -> {
-                _rightButtonTextRes.value = R.string.label_start
-                runStopwatch(coroutineScope, STOPWATCH_DELAY_TIME) { milliseconds ->
-                    _timeText.value = milliseconds.HHmmssFormatString
-                }
-            }
+    fun onClickRightButton() {
+        stopwatchButtonState = when (stopwatchButtonState) {
+            StopwatchButtonState.INIT -> StopwatchButtonState.START
+            StopwatchButtonState.START -> StopwatchButtonState.STOP
+            StopwatchButtonState.STOP -> StopwatchButtonState.CONTINUE
+            StopwatchButtonState.CONTINUE -> StopwatchButtonState.STOP
+            StopwatchButtonState.RESET -> StopwatchButtonState.START
         }
     }
-
 
     fun onClickLeftButton() {
-        _rightButtonTextRes.value = R.string.label_start
-        stopwatchLogicUseCase.resetStopwatch(coroutineScope) { milliseconds ->
-            _timeText.value = milliseconds.HHmmssFormatString
-        }
+        stopwatchButtonState = StopwatchButtonState.RESET
     }
+
+    enum class StopwatchButtonState {
+        INIT, START, STOP, CONTINUE, RESET
+    }
+
 
 }
--- domain/src/main/java/omarbradley/com/domain/usecase/stopwatch/StopwatchLogicUsecase.kt
+++ domain/src/main/java/omarbradley/com/domain/usecase/stopwatch/StopwatchLogicUsecase.kt
@@ -14,13 +14,7 @@ class StopwatchLogicUsecase {
 
     private var stopwatchJob: Job? = null
 
-    var stopwatchState: StopwatchState = StopwatchState.STOP
-        private set(value) {
-            field = value
-        }
-
     fun runStopwatch(coroutineScope: CoroutineScope, delayTimeMillis: Long, subscribe: (Long) -> Unit) {
-        stopwatchState = StopwatchState.START
         stopwatchJob = coroutineScope.launch {
             generateSequence(seed, Long::inc)
                 .asFlow()
@@ -34,22 +28,16 @@ class StopwatchLogicUsecase {
 
     fun stopStopwatch(coroutineScope: CoroutineScope) {
         coroutineScope.launch {
-            stopwatchState = StopwatchState.STOP
             stopwatchJob?.cancelAndJoin()
         }
     }
 
     fun resetStopwatch(coroutineScope: CoroutineScope, subscribe: (Long) -> Unit) {
         coroutineScope.launch {
-            stopwatchState = StopwatchState.RESET
             stopwatchJob?.cancelAndJoin()
             seed = 0L
             subscribe(seed)
         }
     }
 
-}
-
-enum class StopwatchState {
-    START, STOP, RESET
 }
